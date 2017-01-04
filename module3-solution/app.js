f6be93fcd014b80fbe32e1799a9e4fb12451975e
davidsanchezcrespillo <davidsanchezcrespillo@users.noreply.github.com>
2017-01-04 14:44:04 +0100

    Non-working intermediate commit

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,16 +4,39 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItems);
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController() {
   var mv = this;
+  
+  mv.found = [];
 }
 
 function MenuSearchService() {
   this.getMatchedMenuItems = function(searchTerm) {
-    
+    var endPointUrl = "https://davids-restaurant.herokuapp.com/menu_items.json";
+    return $http(endPointUrl).then(function (result) {
+      // process result and only keep items that match
+      var foundItems = result;
+
+	  console.log(foundItems);
+      // return processed items
+      return foundItems;
+    });    
   };
+}
+
+function FoundItems() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+	scope: {
+      found: '<',
+	  onRemove: '&'
+	}
+  };
+  
+  return ddo;
 }
 
 })();
