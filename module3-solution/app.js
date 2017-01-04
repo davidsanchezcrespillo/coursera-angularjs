238a3d03f696165ff378977a4958b3495542728e
davidsanchezcrespillo <davidsanchezcrespillo@users.noreply.github.com>
2017-01-04 16:05:04 +0100

    Yet another intermediate commit

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,6 +3,7 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
+.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/')
 .service('MenuSearchService', MenuSearchService);
 //.directive('foundItems', FoundItems);
 
@@ -10,30 +11,40 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var mv = this;
 
+  mv.searchTerm = "";
+
   mv.found = [];
 
   mv.removeItem = function(index) {
     
   }
 
-  mv.getMatchedMenuItems = function(searchTerm) {
-    return MenuSearchService.getMatchedMenuItems(searchTerm);
+  mv.getMatchedMenuItems = function() {
+    mv.found = MenuSearchService.getMatchedMenuItems(mv.searchTerm);
   }
 
 }
 
-MenuSearchService.$inject = ['$http'];
-function MenuSearchService($http) {
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
   this.getMatchedMenuItems = function(searchTerm) {
-    var endPointUrl = "https://davids-restaurant.herokuapp.com/menu_items.json";
-    return $http(endPointUrl).then(function (result) {
+    var endPointUrl = ApiBasePath + "menu_items.json";
+	var response = $http({
+      method: "GET",
+	  url: endPointUrl
+	});
+    return response.then(function (result) {
       // process result and only keep items that match
-      var foundItems = result;
+      var foundItems = this.processMenuItems(result.data.menu_items, searchTerm);
 
 	  console.log(foundItems);
       // return processed items
       return foundItems;
     });    
+  };
+  
+  this.processMenuItems = function(menuItems, searchTerm) {
+    return menuItems;
   };
 }
 
