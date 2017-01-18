9a99559acb2307b139fe9ddc34d3e1f24cc73f4f
davidsanchezcrespillo <davidsanchezcrespillo@users.noreply.github.com>
2017-01-18 14:51:45 +0100

    Menu existence check and persistence of data after signing up

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -4,14 +4,24 @@
   angular.module('public')
   .controller('SignupController', SignupController);
 
-  SignupController.$inject = ["UserService"];
-  function SignupController(UserService) {
+  SignupController.$inject = ["UserService", "$http", "ApiPath"];
+  function SignupController(UserService, $http, ApiPath) {
     var $ctrl = this;
     $ctrl.submitted = false;
+    $ctrl.user = UserService.getUserPreferences();
 
     $ctrl.submit = function() {
       UserService.setUserPreferences($ctrl.user);
-      $ctrl.submitted = true;
+      var short_name = $ctrl.user.menu_number;
+      $http.get(ApiPath + '/menu_items/' + short_name + '.json')
+      .success(function() {
+        $ctrl.submitted = true;
+        $ctrl.noSuchMenu = false;
+      })
+      .error(function() {
+        $ctrl.submitted = false;
+        $ctrl.noSuchMenu = true;
+      });
     }
   }
 
